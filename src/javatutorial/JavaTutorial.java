daebff42a6f9938442b60d745219662274b850df
PatrickMarcos <patrickmarcosgenesio@gmail.com>
2019-02-10 17:18:24 -0200

    java_tutorial

---
+++ src/javatutorial/JavaTutorial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatutorial;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author patrick
+ */
+
+public class JavaTutorial {
+    public static void main(String[] args) {
+          byte x = 10;
+          float f = 2.5f;
+          double z = 10.5678;
+          Locale.setDefault(Locale.US);
+          System.out.printf("%.2f",z);
+          
+    }
+}
---
+++ src/javatutorial/Pessoa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatutorial;
+
+/**
+ *
+ * @author patrick
+ */
+public class Pessoa {
+
+    public Pessoa() {
+        
+    }
+    
+    public Double andar(String nome, Double distancia){
+       
+        return distancia;
+    }
+}
